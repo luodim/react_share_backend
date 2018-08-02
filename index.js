54ad1888e657b9c7637a8693230974df53bf68c5
luodim <luodim@gmail.com>
2018-08-02 14:32:54 +0800

    Add files via upload
    init project

---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+get home page data list according to pageIndex,
+get is in task status according to userId
+*/
+function getHomeList(pageIndex, userId) {
+
+}
+
+/*
+get task page data list according to userId
+*/
+function getTaskList(userId) {
+
+}
+
+/*
+update task list data according userId and unionId,
+action indicate the action to operate database(update, delete, add)
+*/
+function updateTaskList(userId, unionId, action) {
+
+}
+
+/*
+upload the data as form to server
+*/
+function uploadInfo() {
+
+}
+
+/*
+verify the validation of userId in login page
+*/
+function verifyUserId() {
+
+}
+
+/*
+for admin only, can update user table according to userId and action
+action can be update || delete || add
+*/
+function updateUserList(userId, action) {
+
+}
+
